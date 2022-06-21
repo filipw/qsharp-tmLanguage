5581a3ed802c50b49dd5a4b750b6eb5e6ed621f0
filipw <wojcieszyn@gmail.com>
2022-06-21 20:35:16 +0200

    restructure code

---
+++ test/callabale.declaration.tests.ts
@@ -0,0 +1,49 @@
+import { should } from 'chai';
+import { tokenize, createToken, Token } from './utils/tokenize';
+
+describe("Callable", () => {
+    before(() => { should(); });
+
+    it("Typed argument", async () => {
+        const tokens = await tokenize(`function Subarray<'T>(indices : Int[], array : 'T[]) : 'T[]`);
+        tokens.should.deep.equal([
+            createToken("function", "keyword.other.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("Subarray", "entity.name.callable.qsharp"),
+            createToken("<", "punctuation.definition.typeparameters.begin.qsharp"),
+            createToken("'T", "entity.name.type.type-parameter.qsharp"),
+            createToken(">", "punctuation.definition.typeparameters.end.qsharp"),
+            createToken("(indices : ", "source.qsharp"),
+            createToken("Int", "storage.type.qsharp"),
+            createToken("[], array : '", "source.qsharp"),
+            createToken("T", "support.function.quantum.qsharp"),
+            createToken("[])", "source.qsharp"),
+            createToken(" : '", "source.qsharp"),
+            createToken("T", "support.function.quantum.qsharp"),
+        ]);
+    });
+
+    it("Twp typed arguments", async () => {
+        const tokens = await tokenize(`operation Delay<'T, 'U>(op : ('T => 'U), arg : 'T, aux : Unit) : 'U`);
+        tokens.should.deep.equal([
+            createToken("operation", "keyword.other.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("Delay", "entity.name.callable.qsharp"),
+            createToken("<", "punctuation.definition.typeparameters.begin.qsharp"),
+            createToken("'T", "entity.name.type.type-parameter.qsharp"),
+            createToken(",", "punctuation.separator.comma.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("'U", "entity.name.type.type-parameter.qsharp"),
+            createToken(">", "punctuation.definition.typeparameters.end.qsharp"),
+            createToken("(op : ('", "source.qsharp"),
+            createToken("T", "support.function.quantum.qsharp"),
+            createToken(" => 'U)", "source.qsharp"),
+            createToken(", arg : '", "source.qsharp"),
+            createToken("T", "support.function.quantum.qsharp"),
+            createToken(", aux : ", "source.qsharp"),
+            createToken("Unit", "storage.type.qsharp"),
+        ]);
+    });
+
+    
+});
---
+++ test/callabale.invocation.tests.ts
@@ -0,0 +1,26 @@
+import { should } from 'chai';
+import { tokenize, createToken, Token } from './utils/tokenize';
+
+describe("Callable invocation", () => {
+    before(() => { should(); });
+
+    it("Two arguments", async () => {
+        const tokens = await tokenize("ApplyFermionicSWAP(left, right);");
+        tokens.should.deep.equal([
+            createToken("ApplyFermionicSWAP", "entity.name.callable.qsharp"),
+            createToken("(left, right)", "source.qsharp")
+        ]);
+    });
+
+    it("String argument", async () => {
+        const tokens = await tokenize(`EqualityFactI(n, 0, "syndrome failure");`);
+        tokens.should.deep.equal([
+            createToken("EqualityFactI", "entity.name.callable.qsharp"),
+            createToken("(n, 0, ", "source.qsharp"),
+            createToken("\"", "string.quoted.double.qsharp"),
+            createToken("syndrome failure", "string.quoted.double.qsharp"),
+            createToken("\"", "string.quoted.double.qsharp"),
+            createToken(")", "source.qsharp"),
+        ]);
+    });
+});
