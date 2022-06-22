2ed61d986bca28772d8957cbc672cd87d6513959
filipw <wojcieszyn@gmail.com>
2022-06-22 11:33:00 +0200

    added simple code block

---
+++ test/block.tests.ts
@@ -0,0 +1,42 @@
+import { should } from 'chai';
+import { tokenize, createToken, Token } from './utils/tokenize';
+
+describe("Code blocks", () => {
+    before(() => { should(); });
+
+    it("Simple parameterless operation", async () => {
+        const code = `operation AllocateAndMeasure() : Result {
+use qubit = Qubit();
+Message("Performing measurement");
+return M(qubit);
+}
+        `;
+        const tokens = await tokenize(code);
+        tokens.should.deep.equal([
+            createToken("operation", "keyword.other.callable.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("AllocateAndMeasure", "entity.name.function.qsharp"),
+            createToken("(", "punctuation.parenthesis.open.qsharp"),
+            createToken(")", "punctuation.parenthesis.close.qsharp"),
+            createToken(" : ", "source.qsharp"),
+            createToken("Result", "storage.type.qsharp"),
+            createToken(" {", "source.qsharp"),
+            createToken("use", "keyword.control.qsharp"),
+            createToken(" qubit = ", "source.qsharp"),
+            createToken("Qubit", "storage.type.qsharp"),
+            createToken("();", "source.qsharp"),
+            createToken("Message", "entity.name.function.qsharp"),
+            createToken("(", "punctuation.parenthesis.open.qsharp"),
+            createToken("\"", "string.quoted.double.qsharp"),
+            createToken("Performing measurement", "string.quoted.double.qsharp"),
+            createToken("\"", "string.quoted.double.qsharp"),
+            createToken(")", "punctuation.parenthesis.close.qsharp"),
+            createToken(";", "source.qsharp"),
+            createToken("return", "keyword.control.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("M", "support.function.quantum.qsharp"),
+            createToken("(qubit);", "source.qsharp"),
+            createToken("}", "source.qsharp"),
+        ]);
+    });
+});
