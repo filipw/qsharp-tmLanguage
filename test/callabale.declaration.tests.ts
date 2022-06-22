83ee3d54b6e46994d070e695b3ee0236f06d283a
filipw <wojcieszyn@gmail.com>
2022-06-22 07:36:22 +0200

    added parameter list

--- test/callabale.declaration.tests.ts
+++ test/callabale.declaration.tests.ts
@@ -4,7 +4,7 @@ import { tokenize, createToken, Token } from './utils/tokenize';
 describe("Callable declarations", () => {
     before(() => { should(); });
 
-    it("Typed argument", async () => {
+    it("Typed parameter", async () => {
         const tokens = await tokenize(`function Subarray<'T>(indices : Int[], array : 'T[]) : 'T[]`);
         tokens.should.deep.equal([
             createToken("function", "keyword.other.callable.qsharp"),
@@ -13,17 +13,32 @@ describe("Callable declarations", () => {
             createToken("<", "punctuation.definition.typeparameters.begin.qsharp"),
             createToken("'T", "entity.name.type.type-parameter.qsharp"),
             createToken(">", "punctuation.definition.typeparameters.end.qsharp"),
-            createToken("(indices : ", "source.qsharp"),
+            createToken("(", "punctuation.parenthesis.open.qsharp"),
+            createToken("indices", "entity.name.variable.parameter.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken(":", "punctuation.separator.colon.qsharp"),
+            createToken(" ", "source.qsharp"),
             createToken("Int", "storage.type.qsharp"),
-            createToken("[], array : '", "source.qsharp"),
+            createToken("[", "punctuation.squarebracket.open.qsharp"),
+            createToken("]", "punctuation.squarebracket.close.qsharp"),
+            createToken(",", "punctuation.separator.comma.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("array", "entity.name.variable.parameter.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken(":", "punctuation.separator.colon.qsharp"),
+            createToken(" '", "source.qsharp"),
             createToken("T", "support.function.quantum.qsharp"),
-            createToken("[])", "source.qsharp"),
+            createToken("[", "punctuation.squarebracket.open.qsharp"),
+            createToken("]", "punctuation.squarebracket.close.qsharp"),
+            createToken(")", "punctuation.parenthesis.close.qsharp"),
             createToken(" : '", "source.qsharp"),
             createToken("T", "support.function.quantum.qsharp"),
+            createToken("[", "punctuation.squarebracket.open.qsharp"),
+            createToken("]", "punctuation.squarebracket.close.qsharp"),
         ]);
     });
 
-    it("Twp typed arguments", async () => {
+    it("Twp typed parameters", async () => {
         const tokens = await tokenize(`operation Delay<'T, 'U>(op : ('T => 'U), arg : 'T, aux : Unit) : 'U`);
         tokens.should.deep.equal([
             createToken("operation", "keyword.other.callable.qsharp"),
@@ -35,9 +50,14 @@ describe("Callable declarations", () => {
             createToken(" ", "source.qsharp"),
             createToken("'U", "entity.name.type.type-parameter.qsharp"),
             createToken(">", "punctuation.definition.typeparameters.end.qsharp"),
-            createToken("(op : ('", "source.qsharp"),
+            createToken("(", "punctuation.parenthesis.open.qsharp"),
+            createToken("op", "entity.name.variable.parameter.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken(":", "punctuation.separator.colon.qsharp"),
+            createToken(" ('", "source.qsharp"),
             createToken("T", "support.function.quantum.qsharp"),
-            createToken(" => 'U)", "source.qsharp"),
+            createToken(" => 'U", "source.qsharp"),
+            createToken(")", "punctuation.parenthesis.close.qsharp"),
             createToken(", arg : '", "source.qsharp"),
             createToken("T", "support.function.quantum.qsharp"),
             createToken(", aux : ", "source.qsharp"),
