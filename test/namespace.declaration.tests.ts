a9b331149f78ba496b043757159f2cc2b8e7d762
filipw <wojcieszyn@gmail.com>
2022-06-25 20:15:11 +0200

    namespace declaration tests

---
+++ test/namespace.declaration.tests.ts
@@ -0,0 +1,64 @@
+import { should } from 'chai';
+import { tokenize, createToken, Token } from './utils/tokenize';
+
+describe("Namespace declarations", () => {
+    before(() => { should(); });
+
+    it("Empty single line", async () => {
+        const tokens = await tokenize(`namespace Microsoft.Quantum.Samples {}`);
+        tokens.should.deep.equal([
+            createToken("namespace", "keyword.other.namespace.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("Microsoft", "entity.name.type.namespace.qsharp"),
+            createToken(".", "punctuation.accessor.qsharp"),
+            createToken("Quantum", "entity.name.type.namespace.qsharp"),
+            createToken(".", "punctuation.accessor.qsharp"),
+            createToken("Samples", "entity.name.type.namespace.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("{", "punctuation.curlybrace.open.qsharp"),
+            createToken("}", "punctuation.curlybrace.close.qsharp"),
+        ]);
+    });
+
+    it("Empty multi line", async () => {
+        const tokens = await tokenize(`namespace Microsoft.Quantum.Samples
+{
+}`);
+        tokens.should.deep.equal([
+            createToken("namespace", "keyword.other.namespace.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("Microsoft", "entity.name.type.namespace.qsharp"),
+            createToken(".", "punctuation.accessor.qsharp"),
+            createToken("Quantum", "entity.name.type.namespace.qsharp"),
+            createToken(".", "punctuation.accessor.qsharp"),
+            createToken("Samples", "entity.name.type.namespace.qsharp"),
+            createToken("{", "punctuation.curlybrace.open.qsharp"),
+            createToken("}", "punctuation.curlybrace.close.qsharp"),
+        ]);
+    });
+
+    it("With no-op code", async () => {
+        const tokens = await tokenize(`namespace Microsoft.Quantum.Samples {
+    open Microsoft.Quantum.Arithmetic; 
+    // ...
+}
+        `);
+        tokens.should.deep.equal([
+            createToken("namespace", "keyword.other.namespace.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("Microsoft", "entity.name.type.namespace.qsharp"),
+            createToken(".", "punctuation.accessor.qsharp"),
+            createToken("Quantum", "entity.name.type.namespace.qsharp"),
+            createToken(".", "punctuation.accessor.qsharp"),
+            createToken("Samples", "entity.name.type.namespace.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("{", "punctuation.curlybrace.open.qsharp"),
+            createToken("    ", "source.qsharp"),
+            createToken("open", "keyword.other.qsharp"),
+            createToken(" Microsoft.Quantum.Arithmetic; ", "source.qsharp"),
+            createToken("    ", "source.qsharp"),
+            createToken("// ...", "comment.line.double-slash"),
+            createToken("}", "punctuation.curlybrace.close.qsharp"),
+        ]);
+    });
+});
