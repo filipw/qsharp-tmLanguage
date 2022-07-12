4b77da8694fa7c1bb3ba812da15ac4db4f06d947
filipw <wojcieszyn@gmail.com>
2022-07-12 17:13:09 +0200

    added mutable handling

--- test/declaration.local.tests.ts
+++ test/declaration.local.tests.ts
@@ -4,10 +4,23 @@ import { tokenize, createToken, Token } from './utils/tokenize';
 describe("Local declarations", () => {
     before(() => { should(); });
 
-    it("Simple numeric", async () => {
+    it("Immutable simple numeric", async () => {
         const tokens = await tokenize(`let foo = 1.0;`);
         tokens.should.deep.equal([
             createToken("let", "keyword.other.let.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("foo", "entity.name.variable.local.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("=", "keyword.operator.assignment.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("1.0", "constant.numeric.decimal.qsharp"),
+        ]);
+    });
+
+    it("Mutable simple numeric", async () => {
+        const tokens = await tokenize(`mutable foo = 1.0;`);
+        tokens.should.deep.equal([
+            createToken("mutable", "keyword.other.mutable.qsharp"),
             createToken(" ", "source.qsharp"),
             createToken("foo", "entity.name.variable.local.qsharp"),
             createToken(" ", "source.qsharp"),
