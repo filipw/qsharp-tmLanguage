4d4d4f3df045beca524326d80780d208ac09fe5b
filipw <wojcieszyn@gmail.com>
2022-07-13 16:24:40 +0200

    more local declaration tests

--- test/declaration.local.tests.ts
+++ test/declaration.local.tests.ts
@@ -30,7 +30,7 @@ describe("Local declarations", () => {
         ]);
     });
 
-    it("Pauli array", async () => {
+    it("Immutable array", async () => {
         const tokens = await tokenize("let pauliGroup = [PauliX, PauliY, PauliZ];");
         tokens.should.deep.equal([
             createToken("let", "keyword.other.let.qsharp"),
@@ -51,10 +51,48 @@ describe("Local declarations", () => {
         ]);
     });
 
-    it("From callable invocation", async () => {
+
+    it("Mutable array", async () => {
+        const tokens = await tokenize("mutable pauliGroup = [PauliX, PauliY, PauliZ];");
+        tokens.should.deep.equal([
+            createToken("mutable", "keyword.other.mutable.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("pauliGroup", "entity.name.variable.local.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("=", "keyword.operator.assignment.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("[", "punctuation.squarebracket.open.qsharp"),
+            createToken("PauliX", "constant.language.pauli.qsharp"),
+            createToken(",", "punctuation.separator.comma.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("PauliY", "constant.language.pauli.qsharp"),
+            createToken(",", "punctuation.separator.comma.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("PauliZ", "constant.language.pauli.qsharp"),
+            createToken("]", "punctuation.squarebracket.close.qsharp"),
+        ]);
+    });
+
+    it("Immutable from callable invocation", async () => {
         const tokens = await tokenize("let result = M(qubit);");
         tokens.should.deep.equal([
             createToken("let", "keyword.other.let.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("result", "entity.name.variable.local.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("=", "keyword.operator.assignment.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("M", "entity.name.function.qsharp"),
+            createToken("(", "punctuation.parenthesis.open.qsharp"),
+            createToken("qubit", "variable.other.readwrite.qsharp"),
+            createToken(")", "punctuation.parenthesis.close.qsharp"),
+        ]);
+    });
+
+    it("Mutable from callable invocation", async () => {
+        const tokens = await tokenize("mutable result = M(qubit);");
+        tokens.should.deep.equal([
+            createToken("mutable", "keyword.other.mutable.qsharp"),
             createToken(" ", "source.qsharp"),
             createToken("result", "entity.name.variable.local.qsharp"),
             createToken(" ", "source.qsharp"),
