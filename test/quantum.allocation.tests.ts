b83fddb3e7c7d4fa05db65f222bc8b86c2fb7c82
filipw <wojcieszyn@gmail.com>
2022-07-24 14:50:41 +0200

    added basic borrow support

--- test/quantum.allocation.tests.ts
+++ test/quantum.allocation.tests.ts
@@ -19,10 +19,41 @@ describe("Quantum: Allocation", () => {
         ]);
     });
 
+    it("Borrow single qubit", async () => {
+        const tokens = await tokenize(`borrow a = Qubit();`);
+        tokens.should.deep.equal([
+            createToken("borrow", "keyword.other.borrow.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("a", "entity.name.variable.local.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("=", "keyword.operator.assignment.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("Qubit", "entity.name.function.qsharp"), // todo: should this be storage.type.qsharp or is this OK?
+            createToken("(", "punctuation.parenthesis.open.qsharp"),
+            createToken(")", "punctuation.parenthesis.close.qsharp"),
+        ]);
+    });
+
     it("Use qubit array", async () => {
         const tokens = await tokenize(`use a = Qubit[2];`);
         tokens.should.deep.equal([
             createToken("use", "keyword.other.use.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("a", "entity.name.variable.local.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("=", "keyword.operator.assignment.qsharp"),
+            createToken(" ", "source.qsharp"),
+            createToken("Qubit", "storage.type.qsharp"), 
+            createToken("[", "punctuation.squarebracket.open.qsharp"),
+            createToken("2", "constant.numeric.decimal.qsharp"),
+            createToken("]", "punctuation.squarebracket.close.qsharp"),
+        ]);
+    });
+
+    it("Borrow qubit array", async () => {
+        const tokens = await tokenize(`borrow a = Qubit[2];`);
+        tokens.should.deep.equal([
+            createToken("borrow", "keyword.other.borrow.qsharp"),
             createToken(" ", "source.qsharp"),
             createToken("a", "entity.name.variable.local.qsharp"),
             createToken(" ", "source.qsharp"),
