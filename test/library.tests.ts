0aaf964b4cff2f1733f272ef5bb85854aeea7f2f
filipw <wojcieszyn@gmail.com>
2022-08-14 07:26:08 +0200

    added Y Z tests

--- test/library.tests.ts
+++ test/library.tests.ts
@@ -19,6 +19,16 @@ describe("Library", () => {
         tokens.should.deep.equal([createToken("X", "support.function.quantum.qsharp")]);
     });
 
+    it("Y", async () => {
+        const tokens = await tokenize("Y(qubit)");
+        tokens.should.deep.equal([createToken("Y", "support.function.quantum.qsharp")]);
+    });
+
+    it("Z", async () => {
+        const tokens = await tokenize("Z(qubit)");
+        tokens.should.deep.equal([createToken("Z", "support.function.quantum.qsharp")]);
+    });
+
     it("CNOT", async () => {
         const tokens = await tokenize("CNOT(qubit1, qubit2)");
         tokens.should.deep.equal([createToken("CNOT", "support.function.quantum.qsharp")]);
